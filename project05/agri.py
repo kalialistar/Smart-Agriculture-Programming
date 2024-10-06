9c833959646b196c274bbface03dc716a174a3e5
서보성, 최지훈 <jerry1174@naver.com>
2024-10-06 18:28:16 +0900

    Update agri.py

--- project05/agri.py
+++ project05/agri.py
@@ -3,22 +3,6 @@ import streamlit as st
 import pandas as pd
 from datetime import date, timedelta
 
-# 파일 업로드 기능 추가
-uploaded_file = st.file_uploader("CSV 파일을 선택하세요", type="csv")
-if uploaded_file is not None:
-    # 업로드한 파일을 데이터프레임으로 읽기
-    df = pd.read_csv(uploaded_file)
-    st.write(df)  # 데이터프레임 출력
-
-# 데이터 로드 및 세션 상태에서 관리
-file_path = 'project05/data_file.csv'
-
-if 'data' not in st.session_state:
-
-    st.session_state['data'] = pd.read_csv(file_path, encoding='euc-kr')
-
-data = st.session_state['data']
-
 # Streamlit 애플리케이션
 st.title('경기도 농기계 임대 시스템')
 
